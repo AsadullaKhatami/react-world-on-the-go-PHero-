a0bfc98e1192883c327e19c8e50e3da7263304f1
Asadulla Khatami <asadullakhatami@gmail.com>
2025-12-25 16:39:09 +0600

    simple react rest countries

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,15 +1,37 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Country from "../country/country";
 import "./countries.css";
 
 const Countries = ({ fetchCountry }) => {
+  const [visitCountry, setVisitCountry] = useState([]);
+
+  const alreadyVisited = (data) => {
+    const newVisitCountry = data;
+    setVisitCountry([...visitCountry, newVisitCountry]);
+  };
+
   const country = use(fetchCountry);
   return (
     <div>
       <h1>countries: {country.length}</h1>
       <div className="countries">
+        {visitCountry.map((data) => (
+          <Country key={data.cca2} data={data}></Country>
+        ))}
+      </div>
+      <h3>already visited country: {visitCountry.length}</h3>
+      <ol>
+        {visitCountry.map((country) => (
+          <li>{country.name.common}</li>
+        ))}
+      </ol>
+      <div className="countries">
         {country.map((data) => (
-          <Country data={data}></Country>
+          <Country
+            key={data.cca2}
+            data={data}
+            alreadyVisited={alreadyVisited}
+          ></Country>
         ))}
       </div>
     </div>
--- src/components/country/country.jsx
+++ src/components/country/country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./country.css";
 
-const Country = ({ data }) => {
+const Country = ({ data, alreadyVisited, }) => {
   //   console.log(data.name.common);
   const [visited, setVisited] = useState(false);
   const handelVisited = () => {
@@ -11,10 +11,11 @@ const Country = ({ data }) => {
       setVisited(true);
     }
     // console.log(visited);
+    alreadyVisited(data);
   };
 
   return (
-    <div className={`country ${visited && 'country-visited'}`}>
+    <div className={`country ${visited && "country-visited"}`}>
       <h5>name: {data.name.common}</h5>
       <img src={data.flags.png} alt={data.flags.alt} />
       <p>population: {data.population}</p>
